fdbbc2e197a18a7a65e990b1560035ad2b1d3ce1
Mommy <mommy@Mommys-iMac.local>
2021-12-05 18:04:04 -0600

    fix: trial

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/trial/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/trial/app.py
@@ -13,6 +13,7 @@ from opentelemetry.sdk.trace.export import (
     ConsoleSpanExporter,
     SimpleExportSpanProcessor,
 )
+import logging
 
 trace.set_tracer_provider(TracerProvider())
 trace.get_tracer_provider().add_span_processor(
